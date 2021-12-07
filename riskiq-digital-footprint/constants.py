2d85c292d150936dd13999ccf6693daf1497dba9
cs-sujeet-ahirrao <46916494+cs-sujeet-ahirrao@users.noreply.github.com>
2021-12-07 18:41:48 +0530

    First Commit

---
+++ riskiq-digital-footprint/constants.py
@@ -0,0 +1,20 @@
+"""
+   Copyright start
+  Copyright (C) 2008 - 2021 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end
+"""
+
+PARAM_MAP = {
+    "Domain": 'DOMAIN',
+    "Host": "HOST",
+    "IP Address": "IP_ADDRESS",
+    "IP Block": "IP_BLOCK",
+    "ASN": "AS",
+    "Page": "PAGE",
+    "SSL Cert": "SSL_CERT",
+    "Contact": "CONTACT",
+    "Self Hosted Resource": "SELF_HOSTED_RESOURCE",
+    "ThirdParty Hosted Resource": "THIRDPARTY_HOSTED_RESOURCE"
+}
